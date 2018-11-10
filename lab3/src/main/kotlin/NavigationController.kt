097c70b7629a892c25f1e1dd4b7c5c811e587228
Sky731 <Ivan4784@mail.ru>
2018-11-10 14:14:36 +0300

    add the simplest authorization system

--- lab3/src/main/kotlin/NavigationController.kt
+++ lab3/src/main/kotlin/NavigationController.kt
@@ -4,8 +4,4 @@ class NavigationController : Serializable {
 
   fun moveToEssential() = "essential?faces-redirect=true"
   fun moveToIndex() = "index?faces-redirect=true"
-
-  companion object {
-    private const val serialVersionUID = 1L
-  }
 }
