1cc02fdafcab0e0983f7d91f5dfc69b82a6c056e
Sky4784 <ivan4784@mail.ru>
2018-11-10 19:31:42 +0300

    add user class

--- lab3/src/main/kotlin/RequestResult.kt
+++ lab3/src/main/kotlin/RequestResult.kt
@@ -1,5 +1,23 @@
 import java.io.Serializable
+import javax.persistence.*
 
-class RequestResult(val x: Double, val y: Double, val r: Double, val isHit: Boolean) : Serializable {
-  constructor() : this(0.0, 0.0, 0.0, false)
+@Entity
+@Table(name = "result")
+class RequestResult() : Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  var id: Long? = null
+
+  var x: Double = 0.0
+  var y: Double = 0.0
+  var r: Double = 0.0
+  var isHit: Boolean = false
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  var user: User? = null
+
+  constructor(x: Double, y: Double, r: Double, isHit: Boolean) : this()
+
 }
--- lab3/src/main/kotlin/User.kt
+++ lab3/src/main/kotlin/User.kt
@@ -1,1 +1,18 @@
-class User(var id: Long = 0, var username: String? = null, var password: String? = null)
+import java.io.Serializable
+import javax.persistence.*
+
+@Entity
+@Table(name = "user")
+class User : Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  var id: Long? = null
+
+  var username: String? = null
+  var password: String? = null
+
+  @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
+  var history: List<RequestResult>? = null
+
+}
